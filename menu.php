a98cb1573dcb31a640fcf916600e151b3f1f197c
Yonmoji-X <yametu55@gmail.com>
2024-07-18 23:44:09 +0900

    demo版

---
+++ menu.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="tmplt_select.php">チェック項目一覧</a>
+            <a class="navbar-brand" href="tmplt_index.php">チェック項目登録</a>
+            <a class="navbar-brand" href="member_index.php">従業員登録</a>
+            <a class="navbar-brand" href="member_select.php">従業員一覧</a>
+            <a class="navbar-brand" href="rcrd_select.php">チェック一覧</a>
+            <a class="navbar-brand" href="rcrd_index.php">[チェック]</a>
+            <a class="navbar-brand" href="logout.php">ログアウト</a>
+        </div>
+    </div>
+</nav>
